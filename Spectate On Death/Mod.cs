194417dc680d5e4de06b00731fcab32660007900
Gustavo Ribeiro <pokegustavo@gmail.com>
2022-08-06 17:05:01 -0300

    Adicionar arquivos de projeto.

---
+++ Spectate On Death/Mod.cs
@@ -0,0 +1,20 @@
+﻿using PulsarModLoader;
+
+namespace Spectate_On_Death
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "1.0";
+
+        public override string Author => "pokegustavo";
+
+        public override string ShortDescription => "Enables Spectator when dead";
+
+        public override string Name => "Spectator Mode";
+
+        public override string HarmonyIdentifier()
+        {
+            return "pokegustavo.speactatormode";
+        }
+    }
+}
---
+++ Spectate On Death/Patch.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Spectate_On_Death
+{
+    [HarmonyPatch(typeof(PLCameraMode_LocalPawn), "GetAvailableViewPawn")]
+    class SpectateOnDeath
+    {
+        static void Postfix(PLCameraMode_LocalPawn __instance, ref PLPawn __result)
+        {
+            if (__result == null)
+            {
+                if (__instance.CurrentViewPawn == null)
+                {
+                    __instance.NextViewPawn();
+                }
+                __result = __instance.CurrentViewPawn;
+                if (__result != null) PLGlobal.Instance.SetBottomInfo("", "[<] Spectating: " + __result.GetName() + " [>]", "", "");
+            }
+        }
+    }
+    [HarmonyPatch(typeof(PLCameraMode_LocalPawn), "Tick")]
+    class ButtonOverride
+    {
+        static void Postfix(PLCameraMode_LocalPawn __instance)
+        {
+            if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.Comma))
+            {
+                __instance.PrevViewPawn();
+            }
+            if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.Period))
+            {
+                __instance.NextViewPawn();
+            }
+        }
+    }
+}
